80d9e0c89b83e4b4060b725336c4a57c5a5ccd9d
Aaron Strick <aaronstrick@gmail.com>
2023-11-10 09:06:24 -0800

    nvim

---
+++ config/nvim/lua/plugins.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.4',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {}
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+        ensure_installed = { "lua", "javascript", "typescript", "elm", "html" },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end
+ }
+}
