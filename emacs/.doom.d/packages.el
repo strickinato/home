3123ef0610b33fbf54700a62ceb3641d1e97a7af
Aaron Strick <aaronstrick@gmail.com>
2025-11-03 15:20:28 -0800

    agent-shell on melpa now

--- emacs/.doom.d/packages.el
+++ emacs/.doom.d/packages.el
@@ -78,8 +78,7 @@
    ))
 
 (package! shell-maker)
-(package! acp :recipe (:host github :repo "xenodium/acp.el"))
-(package! agent-shell :recipe (:host github :repo "xenodium/agent-shell"))
+(package! agent-shell)
 
 (package! dwim-shell-command
   ;; To include shell-commands
